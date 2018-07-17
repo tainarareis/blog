6924040298f76af15a79a4842a49e3273d24191b
tainarareis <tainara.s.r@hotmail.com>
2018-07-16 21:56:42 -0300

    Create Posts model controller migration

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,5 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
-
-Route::get('/tasks', function () {
-    $tasks = App\Task::all();
-
-    return view('tasks.index', compact('tasks'));
-});
-
-Route::get('/tasks/{task}', function ($id) {
-    $task = App\Task::where('id', $id)->get();
-
-    return view('tasks.show', compact ('task'));
-});
+Route::get('/tasks', 'TasksController@index');
+Route::get('/tasks/{task}', 'TasksController@show');
+Route::get('/posts', 'PostsController@index');
